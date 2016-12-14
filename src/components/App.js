796353de01ce4daab40b11d1bba381796c4c75a3
coucou_miya <bingbing.yan@efrei.net>
2016-12-14 20:11:51 +0100

    version 11-- override dispatch function to log actions in concole

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,7 @@ import VisibleTodoList from './VisibleTodoList'
 
 const App = ({ params })=>(
     <div>
-        <p>App version 10: To normalize
-            the state shape to ensure data consistency
-            that is important in real-world applications.
-            <br/>To treat my state as a database, to keep 'todos' in an object (rather than an array)
-            indexed by the IDs of the todos
+        <p>App version 11: Override dispatch function to log actions
         </p>
         <AddTodo/>
         <VisibleTodoList />
--- src/configureStore.js
+++ src/configureStore.js
@@ -6,6 +6,18 @@ import todoApp from './reducers'
 import { createStore } from 'redux'
 import throttle from 'lodash/throttle';
 
+const addLoggingToDispatch = (store) => {
+    const rawDispatch = store.dispatch;
+    return (action) => {
+        console.group(action.type);
+        console.log('%c prev state', 'color: gray', store.getState());
+        console.log('%c action', 'color: blue', action);
+        const returnValue = rawDispatch(action);
+        console.log('%c next state', 'color: green', store.getState());
+        console.groupEnd(action.type);
+        return returnValue;
+    };
+};
 
 const configureStore = () => {
     const persistedData = loadState();
@@ -13,6 +25,9 @@ const configureStore = () => {
         todoApp,
         persistedData
     )
+    if (process.env.NODE_ENV !== 'production') {
+        store.dispatch = addLoggingToDispatch(store);
+    }
     store.subscribe(throttle(() => {
         saveState({todos: store.getState().todos});
     }, 1000));
